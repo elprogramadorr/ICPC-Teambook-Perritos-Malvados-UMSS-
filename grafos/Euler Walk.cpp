a9f9a66bd2bf66984e06a25761bd611d9ab2f4c5
Mateo <mateovaldivia3000@gmail.com>
2024-07-21 23:23:47 -0400

    added description

--- grafos/Euler Walk.cpp
+++ grafos/Euler Walk.cpp
@@ -3,9 +3,17 @@ La entrada es un vector (dest, index global de la arista), para grafos no dirigi
 las aristas de ida y vuelta tienen el mismo index global. Reorna un vector de nodos en el Eulerian path/cycle
 con src como nodo inicial. Si no hay solucion, retorna un vector vacio.
 
-Para obtener indices de aristas, añadir .second a s y ret.
+Para obtener indices de aristas, añadir .second a s y ret o usar mapa.
 Para ver si existe respuesta, ver si ret.size() == nedges + 1
 Para ver si existe camino euleriano con (start, end) también ver si ans.back() == end 
+
+Un grafo dirigido tiene un camino euleriano si:
+
+Tiene exactamente un vértice con out-degree − in-degree = 1
+Tiene exactamente un vértice con in-degree − out-degree = 1
+Todos los demás vértices tienen in-degree = out-degree
+El recorrido empieza en el vértice con out-degree − in-degree = 1 
+Correr desde este nodo y no necesito verficar lo demás (si no hay tal nodo correr desde uno con grado de salida > 0)
 
 Time complexity: O(V + E)
 */

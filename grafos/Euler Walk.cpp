29e8739410ee5f9e6d4c344832f9849462330808
elprogramadorr <91036558+elprogramadorr@users.noreply.github.com>
2024-07-26 15:26:53 -0400

    Update Euler Walk.cpp

--- grafos/Euler Walk.cpp
+++ grafos/Euler Walk.cpp
@@ -1,9 +1,3 @@
-#include <bits/stdc++.h>
-#define vi vector<int>
-#define pii pair<int, int>
-#define pb push_back
-using namespace std;
-vector<vector<pii>> G;
 /*
 La entrada es un vector (dest, index global de la arista), para grafos no dirigidos
 las aristas de ida y vuelta tienen el mismo index global. Reorna un vector de nodos en el Eulerian path/cycle
@@ -23,65 +17,24 @@ Correr desde este nodo y no necesito verficar lo demás (si no hay tal nodo corr
 
 Time complexity: O(V + E)
 */
-vector<pair<int,int>> eulerWalk(vector<vector<pii>> &gr, int nedges, int src = 1){
+vi eulerWalk(vector<vector<pii>> &gr, int nedges, int src = 1){
     int n = gr.size();
-    vi D(n), its(n), eu(nedges); 
-    vector<pair<int,int>> s = {{src, -1}};
-    vector<pair<int,int>> ret;
+    vi D(n), its(n), eu(nedges), ret, s = {src};
     D[src]++; // para permitir Euler Paths, no solo ciclos
     while (!s.empty()){
-        int x = s.back().first, y, e, &it = its[x], end = gr[x].size();
+        int x = s.back(), y, e, &it = its[x], end = gr[x].size();
         if (it == end){
-            ret.pb(s.back());
+            ret.pb(x);
             s.pop_back();
             continue;
         }
         tie(y, e) = gr[x][it++];
         if (!eu[e]){
             D[x]--, D[y]++;
-            s.emplace_back(y, e);
+            s.pb(y);
             eu[e] = 1;
         }
     }
     for(int x : D) if(x < 0 || ret.size() != nedges + 1) return {};
     return {ret.rbegin(), ret.rend()};
-}
-int main(){
-    int n,m,t;
-    cin>>t;
-    while(t--){
-        cin>>n>>m;
-        G.resize(n+1);
-        vi deg(n+1,0);
-        for(int i=0;i<m;i++){
-            int a,b;
-            cin>>a>>b;
-            deg[a]++;
-            deg[b]++;
-            G[a].pb({b,i});
-        }
-        int start=-1;
-        int cant=0;
-        for(int i=0;i<n;i++){
-            if(deg[i]&1){
-                cant++;
-                start=i;
-            }
-        }
-        
-        vector<pair<int,int>> ans=eulerWalk(G,m,2);
-        if(ans.size()==m+1){
-            cout<<"Yes"<<"\n";
-            for(auto x:ans){
-                cout<<x.first<<" ";
-            }
-            cout<<"\n";
-        }else{
-            cout<<"No"<<"\n";
-        }
-
-        
-        
-    }
-    return 0;
 }

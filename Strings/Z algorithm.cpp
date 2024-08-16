acc84178f998d612b11e33cb2a50ce1aab00e1e4
elprogramadorr <91036558+elprogramadorr@users.noreply.github.com>
2024-08-15 22:48:02 -0400

    Create Z algorithm.cpp

---
+++ Strings/Z algorithm.cpp
@@ -0,0 +1,18 @@
+// TIme: O(|s|)
+// Maximum length of a substring that begins at position i and is a prefix of the string.
+// abaabca
+// 0 0 1 2 0 0 1
+vi z_function(string s){
+    int n=s.size();
+    vi z(n);
+    int x=0,y=0;
+    for(int i=1;i<n;i++){
+        z[i]=max(0ll,min(z[i-x],y-i+1));
+        while(i+z[i]<n && s[z[i]]==s[i+z[i]]){
+            x=i;
+            y=i+z[i];
+            z[i]++;
+        }
+    }
+    return z;
+}

4600061533dc7034b8fb7e5744c5265aadc63af0
elprogramadorr <91036558+elprogramadorr@users.noreply.github.com>
2024-08-22 00:14:20 -0400

    Update Euler Walk.cpp

--- Graphs/Euler Walk.cpp
+++ Graphs/Euler Walk.cpp
@@ -16,11 +16,12 @@ Todos los demás vértices tienen in-degree = out-degree
 El recorrido empieza en el vértice con out-degree − in-degree = 1 
 Correr desde este nodo y no necesito verficar lo demás (si no hay tal nodo correr desde uno con grado de salida > 0)
 
+Nota.- Volverlo global D,its,eu si corres varias veces (para cada componente conexa)
 Time complexity: O(V + E)
 */
 vi eulerWalk(vector<vector<pii>> &gr, int nedges, int src = 1){
     int n = gr.size();
-    vi D(n), its(n), eu(nedges), ret, s = {src};// cambiar eu a mapa<int,bool> si las aristas no son [0,nedges] o volverlo global
+    vi D(n), its(n), eu(nedges), ret, s = {src};// cambiar eu a mapa<int,bool> si las aristas no son [0,nedges] 
     D[src]++; // para permitir Euler Paths, no solo ciclos
     while (!s.empty()){
         int x = s.back(), y, e, &it = its[x], end = gr[x].size();

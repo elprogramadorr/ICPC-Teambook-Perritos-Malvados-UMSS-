c4db6ea32f342f2cb0c2d68e9afa638bf6ad531c
Mateo <mateovaldivia3000@gmail.com>
2024-08-25 00:36:13 -0400

    tab 2

--- Data Structures/Sparse Table.cpp
+++ Data Structures/Sparse Table.cpp
@@ -1,17 +1,19 @@
 // Time complexity: O(n log n)
-int ST[20][500005];// log2(MAXN) = 20
-void init(vi &a){
-	int n = a.size();
-	for(int i=0;i<n;i++) ST[0][i] = a[i];
-	for(int i=1;(1<<i)<=n;i++){
-		for(int j=0;j+(1<<i)<=n;j++){
-			ST[i][j] = min(ST[i-1][j],ST[i-1][j+(1<<(i-1))]);
-		}
-	}
+int ST[20][500005]; // log2(MAXN) = 20
+
+void init(vi &a) {
+  int n = a.size();
+  for (int i = 0; i < n; i++) ST[0][i] = a[i];
+  for (int i = 1; (1 << i) <= n; i++) {
+    for (int j = 0; j + (1 << i) <= n; j++) {
+      ST[i][j] = min(ST[i-1][j], ST[i-1][j + (1 << (i-1))]);
+    }
+  }
 }
+
 // index-0
-int query(int l, int r){
-	int len = r-l+1;
-	int pot = __lg(len);
-	return min(ST[pot][l],ST[pot][r-(1<<pot)+1]);
+int query(int l, int r) {
+  int len = r - l + 1;
+  int pot = __lg(len);
+  return min(ST[pot][l], ST[pot][r - (1 << pot) + 1]);
 }

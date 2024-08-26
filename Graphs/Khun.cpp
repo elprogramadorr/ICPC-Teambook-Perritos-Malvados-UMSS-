d4e9863a861f69f9d99d01ebf8aec797decace74
Mateo <mateovaldivia3000@gmail.com>
2024-08-25 23:05:58 -0400

    khun

--- Graphs/Khun.cpp
+++ Graphs/Khun.cpp
@@ -1,19 +1,15 @@
 // algoritmo de khun para grafos bipartitos  O(nm)
 const int tam = 100;
-vi G[tam];
+vi G[tam]; // pueden tener mismo indices nodos de distintos grupos
 bool vis[tam];
-int pareja[tam];
+int pareja[tam];// pareja de los nodos de la derecha
 
-bool dfs(int nodo) {
+bool khun(int nodo) {
   if (vis[nodo]) return false;
   vis[nodo] = 1;
   for (auto it : G[nodo]) {
-    if (pareja[it] == -1) { // llego a un nodo sin emparejar
+    if (pareja[it] == -1 || khun(pareja[it])) {
       pareja[it] = nodo;
-      return true;
-    }
-    if (dfs(pareja[it])) {
-      pareja[it] = nodo; // cambio de color rojo con azul
       return true;
     }
   }
@@ -21,19 +17,17 @@ bool dfs(int nodo) {
 }
 
 int main() {
-  int m, a, b;
+  int m, a, b;  
   cin >> m;
   for (int i = 0; i < m; i++) {
     cin >> a >> b; // de izquierda a derecha
     G[a].pb(b);
   }
   memset(pareja, -1, sizeof(pareja));
-  int res = 0;
-  for (int i = 0; i < 4; i++) {
-    memset(vis, false, sizeof(vis));
-    if (dfs(i)) res++;
+  int match = 0;
+  for (int i = 1; i <= n; i++) {
+    memset(vis, false, sizeof(vis)); // no olvidar
+    if (khun(i)) match++; // camino aumentante
   }
-  cout << res << endl;
-
   return 0;
 }

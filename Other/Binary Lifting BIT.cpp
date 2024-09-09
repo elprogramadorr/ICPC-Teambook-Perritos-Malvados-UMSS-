0640e09bca597f11db12f6f521f2f563add10858
elprogramadorr <91036558+elprogramadorr@users.noreply.github.com>
2024-09-09 00:25:16 -0400

    Create Binary Lifting BIT.cpp

---
+++ Other/Binary Lifting BIT.cpp
@@ -0,0 +1,11 @@
+// lower_bound on the prefix sum array
+int kth(int k){
+    int pos=0;
+    for(int i=20;i>=0;i--){
+        if(pos+(1<<i)<tam && T[pos+(1<<i)]<k){
+            k-=T[pos+(1<<i)];
+            pos+=(1<<i);
+        }
+    }
+    return pos+1;
+}

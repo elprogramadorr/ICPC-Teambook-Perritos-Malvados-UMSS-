e10ef83e5b61c02ccca38f03c02ad79ca8993597
Nicolas Mateo Valdivia Viscarra <91036558+elprogramadorr@users.noreply.github.com>
2025-02-08 00:54:59 -0400

    Create XOR bias.cpp

---
+++ Math/XOR bias.cpp
@@ -0,0 +1,32 @@
+// una representacion minima (no redundante) de los números en términos de combinaciones XOR
+// se pueden formar 2 ^ (bias.size()) numeros distintos con el xor de algun subconjunto
+
+vi get_bias(vi v){
+    vi res;
+    for(auto num : v){
+        for(auto it2 : res){
+            num=min(num,num^it2);
+        }
+        if(num) res.pb(num);
+    }
+    return res;
+}
+ 
+vi _merge(vi A, vi B){
+    vi res = A;
+    for(auto num : B){
+        for(auto it2 : res){
+            num=min(num,num^it2);
+        }
+        if(num) res.pb(num);
+    }
+    return res;
+}
+
+// chequear si se puede formar el numero x con xor de algunos elementos de basi
+bool check(vi &basi, int x){
+    for(auto it : basi){
+        x=min(x,x^it);
+    }
+    return x==0;
+}
